59ae0f8b089c6a5f3846cdd54152788fb062dd61
181250140 <181250140@smail.nju.edu.cn>
2020-06-23 22:29:48 +0800

    修改url

--- utils/util.js
+++ utils/util.js
@@ -16,14 +16,6 @@ const formatDate = date => {
   return [year, month, day].map(formatNumber).join('-') 
 }
 
-const formatDate = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-
-  return [year, month, day].map(formatNumber).join('-') 
-}
-
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
